5492cf3c2efbf6f636f389d5cef7932a19aba217
j-j4y <166659170+j-j4y@users.noreply.github.com>
2024-12-05 13:43:33 -0700

    added levels + background images

--- js final/game.js
+++ js final/game.js
@@ -22,13 +22,29 @@ document.addEventListener("keydown", e=> {
         jump();
 }});
 
+let score = 0;
+
 // checks every 100ms to see if obstacle has been hit
 setInterval(() => {
     obst = document.querySelector(".obstacle").getBoundingClientRect();
     duck = document.getElementById("duck").getBoundingClientRect();
-    if(duck.right >= obst.x && duck.bottom > obst.y){
+    if(duck.right >= obst.x && duck.bottom > obst.y) {
         alert("you lost!");
+        score = 0;
+        document.getElementById("container").style.backgroundImage = "" ;
         document.querySelector("obstacle").style.animation ="none";
+    }else if(obst.x <= duck.x) {
+        score += 1;
+        document.getElementById("score").innerHTML = `Score: ${score}` ;
+    }else if(score >= 8) {
+        document.getElementById("container").style.backgroundImage = "url(imgs/morning.jpg)"; //changes to daytime bg
     }
+    if(score >= 10) {
+        document.getElementById("container").style.backgroundImage = "url(imgs/night.jpg)"; //changes to mightt bg
+        // document.getElementById("container").style.backgroundColor = "pink";
+        // console.log("changed color");
+    }
+    
 
 },100) //unit is milliseconds
+
