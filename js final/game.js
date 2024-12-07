619a914b2085eeade85e4f20eeedec6f8e31e91a
j-j4y <166659170+j-j4y@users.noreply.github.com>
2024-12-07 15:45:17 -0700

    changed level change conditions

--- js final/game.js
+++ js final/game.js
@@ -33,19 +33,20 @@ setInterval(() => {
     if(fox.right >= obst.x && fox.bottom > obst.y) { //if player touches sides or top of obstacle, end the game
         alert("you lost!");
         score = 0;
+        document.getElementById("fox").style.backgroundImage = "url(imgs/my_FOX.png)"; //reset to fox when player loses IF secret level reached
         document.getElementById("container").style.backgroundImage = "" ;
         document.querySelector("obstacle").style.animation ="none";
     }else if(obst.x <= fox.x) {
         score += 1;
         document.getElementById("score").innerHTML = `Score: ${score}` ; //increment score and print it to the page
-    }else if(score >= 6) {
+    }else if(score >= 30) {
         document.getElementById("container").style.backgroundImage = "url(imgs/ice_bg.png)"; //changes to secret level bg
         document.getElementById("fox").style.backgroundImage = "url(imgs/penguin.png)"; //changes player at certain score
-    }else if(score >= 4) {
+    }else if(score >= 20) {
         document.getElementById("container").style.backgroundImage = "url(imgs/Summer8.png)"; //changes to level 3 bg
         // document.getElementById("container").style.backgroundColor = "pink";
         // console.log("changed color");
-    }else if(score >= 2) {
+    }else if(score >= 10) {
         document.getElementById("container").style.backgroundImage = "url(imgs/Summer6.png)";
         
     }
